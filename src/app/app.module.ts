f0533814aa95719b8906f9918760bbb9577971dd
macoba69 <carlos.matelunab@gmail.com>
2018-10-11 20:40:18 -0400

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactlistComponent } from './contacts/contactlist/contactlist.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactlistComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
