fc78c4157a6eda7c01fe9ac5bc33358243b91658
macoba69 <carlos.matelunab@gmail.com>
2018-10-14 18:33:27 -0300

    implement all API endpoints and create angular web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ContactlistComponent } from './contacts/contactlist/contactlist.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactlistComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactDetailsComponent
   ],
   imports: [
+    FormsModule,
+    HttpModule,
     BrowserModule
   ],
   providers: [],
